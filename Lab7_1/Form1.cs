8d7db69a96fd595eef9cf6c9aeef1ef0b5bfe73a
Ahmed El-Essawy <ahm.elessawy@gmail.com>
2017-01-17 12:23:20 +0200

    Lab7_1 17-1-2017

--- Lab7_1/Form1.cs
+++ Lab7_1/Form1.cs
@@ -2,13 +2,14 @@
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
-namespace Lab6_1
+namespace Lab7_1
 {
 	public partial class Form1 : Form
 	{
 		string c_name = "ABC Company";
 		string[] yrs = { "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997" };
 		string[] rvn = { "150", "170", "180", "175", "200", "250", "210", "240", "280", "140" };
+		Color color = Color.Blue;
 		public Form1()
 		{
 			InitializeComponent();
@@ -34,8 +35,7 @@ namespace Lab6_1
 			y += 60;
 			Pen pen = new Pen(Color.Black, 1);
 			pen.EndCap = LineCap.ArrowAnchor;
-			Pen penB = new Pen(Color.Blue, 3);
-			Pen penR = new Pen(Color.Red, 5);
+			Pen penR = new Pen(Color.Red, 1);
 			Font font = new Font("Arial", 16);
 			SolidBrush brush = new SolidBrush(Color.Black);
 			Point orgin = new Point(100, this.Height - 100);
@@ -46,21 +46,14 @@ namespace Lab6_1
 			for (int i = 0, cx = orgin.X; i < yrs.Length; i++, cx += 100)
 			{
 				e.Graphics.DrawString(yrs[i], font, brush, cx + 70, orgin.Y + 10);
-				e.Graphics.DrawLine(penR, cx + 100, orgin.Y + 10, cx + 100, orgin.Y - (int.Parse(rvn[i]) - 140 + 10) * 5);
+				e.Graphics.DrawLine(penR, cx + 100, orgin.Y + 8, cx + 100, orgin.Y - (int.Parse(rvn[i]) - 140 + 10) * 5);
 			}
 			for (int i = 140, cy = orgin.Y - 50; i <= 300; i += 10, cy -= 50)
 			{
 				e.Graphics.DrawString(i.ToString(), font, brush, orgin.X - 55, cy - 12);
 				e.Graphics.DrawLine(pen, orgin.X - 8, cy, orgin.X + 1050, cy);
 			}
-			int xx = orgin.X + 100;
-			int yy = orgin.Y - (int.Parse(rvn[0]) - 140 + 10) * 5;
-			for (int i = 0, cx = orgin.X, cy = orgin.Y; i < rvn.Length && i < yrs.Length; i++, cy -= 50, cx += 100)
-			{
-				e.Graphics.DrawLine(penB, xx, yy, cx + 100, orgin.Y - (int.Parse(rvn[i]) - 140 + 10) * 5);
-				xx = cx + 100;
-				yy = orgin.Y - (int.Parse(rvn[i]) - 140 + 10) * 5;
-			}
+			DrawGraph(orgin);
 		}
 		public int DrawStr(string str, int size, int x, int y)
 		{
@@ -86,6 +79,63 @@ namespace Lab6_1
 			Rectangle rect = new Rectangle(x, y, w, h);
 			graphic.DrawRectangle(pen, x, y, w, h);
 			graphic.DrawString(str, font, brush, rect, sf);
+		}
+		public void DrawGraph(Point orgin)
+		{
+			Graphics graphic = this.CreateGraphics();
+			Pen pen = new Pen(color, 3);
+			HatchBrush brushR = new HatchBrush(HatchStyle.BackwardDiagonal, Color.White, Color.FromArgb(175, 255, 0, 0));
+			int xx = orgin.X + 100;
+			int yy = orgin.Y - (int.Parse(rvn[0]) - 140 + 10) * 5;
+			for (int i = 0, cx = orgin.X, cy = orgin.Y; i < rvn.Length && i < yrs.Length; i++, cy -= 50, cx += 100)
+			{
+				graphic.DrawLine(pen, xx, yy, cx + 100, orgin.Y - (int.Parse(rvn[i]) - 140 + 10) * 5);
+				xx = cx + 100;
+				yy = orgin.Y - (int.Parse(rvn[i]) - 140 + 10) * 5;
+				Rectangle rect = new Rectangle(xx - 10, yy, 20, orgin.Y - yy);
+				graphic.FillRectangle(brushR, rect);
+			}
+		}
+		public void changeGraph(Point orgin)
+		{
+			Graphics graphic = this.CreateGraphics();
+			Pen pen = new Pen(color, 3);
+			int xx = orgin.X + 100;
+			int yy = orgin.Y - (int.Parse(rvn[0]) - 140 + 10) * 5;
+			for (int i = 0, cx = orgin.X, cy = orgin.Y; i < rvn.Length && i < yrs.Length; i++, cy -= 50, cx += 100)
+			{
+				graphic.DrawLine(pen, xx, yy, cx + 100, orgin.Y - (int.Parse(rvn[i]) - 140 + 10) * 5);
+				xx = cx + 100;
+				yy = orgin.Y - (int.Parse(rvn[i]) - 140 + 10) * 5;
+			}
+		}
+		private void Form1_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (Control.ModifierKeys == Keys.Control)
+			{
+				Point p = new Point(100, this.Height - 100);
+				switch (e.KeyCode)
+				{
+					case Keys.R:
+						color = Color.Red;
+						break;
+					case Keys.G:
+						color = Color.Green;
+						break;
+					case Keys.B:
+						color = Color.Blue;
+						break;
+				}
+				changeGraph(p);
+			}
+		}
+		private void Form1_MouseUp(object sender, MouseEventArgs e)
+		{
+			int height = (((this.Height - 100) - e.Y) / 5) + 130;
+			if (height <= 300 && height >= 140 && e.X >= 100 && e.X <= 1200)
+			{
+				MessageBox.Show(height.ToString());
+			}
 		}
 	}
 }
