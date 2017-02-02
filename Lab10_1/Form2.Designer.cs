839627ef1525d0b637fc7592f7f74bd5e2f704bb
Ahmed El-Essawy <ahm.elessawy@gmail.com>
2017-02-02 11:10:51 +0200

    Lab10_1 2-2-2017

---
+++ Lab10_1/Form2.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Lab10_1
+{
+	partial class Form2
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose( bool disposing )
+		{
+			if(disposing && ( components != null ))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.radioButton6 = new System.Windows.Forms.RadioButton();
+			this.radioButton5 = new System.Windows.Forms.RadioButton();
+			this.radioButton4 = new System.Windows.Forms.RadioButton();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
+			this.ChooseColor = new System.Windows.Forms.Button();
+			this.tabPage4 = new System.Windows.Forms.TabPage();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			this.tabPage3.SuspendLayout();
+			this.tabPage4.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Controls.Add(this.tabPage3);
+			this.tabControl1.Controls.Add(this.tabPage4);
+			this.tabControl1.Location = new System.Drawing.Point(0, 0);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(296, 181);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.radioButton3);
+			this.tabPage1.Controls.Add(this.radioButton2);
+			this.tabPage1.Controls.Add(this.radioButton1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(288, 155);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Font";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.AutoSize = true;
+			this.radioButton3.Location = new System.Drawing.Point(82, 102);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(60, 17);
+			this.radioButton3.TabIndex = 2;
+			this.radioButton3.Text = "Courier";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButtontab1_CheckedChanged);
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.AutoSize = true;
+			this.radioButton2.Location = new System.Drawing.Point(82, 69);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(46, 17);
+			this.radioButton2.TabIndex = 1;
+			this.radioButton2.Text = "Arial";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButtontab1_CheckedChanged);
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.AutoSize = true;
+			this.radioButton1.Checked = true;
+			this.radioButton1.Location = new System.Drawing.Point(82, 37);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(107, 17);
+			this.radioButton1.TabIndex = 0;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "Time New Roman";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButtontab1_CheckedChanged);
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.radioButton6);
+			this.tabPage2.Controls.Add(this.radioButton5);
+			this.tabPage2.Controls.Add(this.radioButton4);
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(288, 155);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Size";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton6
+			// 
+			this.radioButton6.AutoSize = true;
+			this.radioButton6.Location = new System.Drawing.Point(93, 107);
+			this.radioButton6.Name = "radioButton6";
+			this.radioButton6.Size = new System.Drawing.Size(37, 17);
+			this.radioButton6.TabIndex = 2;
+			this.radioButton6.TabStop = true;
+			this.radioButton6.Text = "24";
+			this.radioButton6.UseVisualStyleBackColor = true;
+			this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButtontab2_CheckedChanged);
+			// 
+			// radioButton5
+			// 
+			this.radioButton5.AutoSize = true;
+			this.radioButton5.Location = new System.Drawing.Point(93, 68);
+			this.radioButton5.Name = "radioButton5";
+			this.radioButton5.Size = new System.Drawing.Size(37, 17);
+			this.radioButton5.TabIndex = 1;
+			this.radioButton5.TabStop = true;
+			this.radioButton5.Text = "20";
+			this.radioButton5.UseVisualStyleBackColor = true;
+			this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButtontab2_CheckedChanged);
+			// 
+			// radioButton4
+			// 
+			this.radioButton4.AutoSize = true;
+			this.radioButton4.Location = new System.Drawing.Point(93, 27);
+			this.radioButton4.Name = "radioButton4";
+			this.radioButton4.Size = new System.Drawing.Size(37, 17);
+			this.radioButton4.TabIndex = 0;
+			this.radioButton4.TabStop = true;
+			this.radioButton4.Text = "16";
+			this.radioButton4.UseVisualStyleBackColor = true;
+			this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButtontab2_CheckedChanged);
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.Controls.Add(this.ChooseColor);
+			this.tabPage3.Location = new System.Drawing.Point(4, 22);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Size = new System.Drawing.Size(288, 155);
+			this.tabPage3.TabIndex = 2;
+			this.tabPage3.Text = "Color";
+			this.tabPage3.UseVisualStyleBackColor = true;
+			// 
+			// ChooseColor
+			// 
+			this.ChooseColor.Location = new System.Drawing.Point(61, 69);
+			this.ChooseColor.Name = "ChooseColor";
+			this.ChooseColor.Size = new System.Drawing.Size(152, 23);
+			this.ChooseColor.TabIndex = 4;
+			this.ChooseColor.Text = "Choose Color";
+			this.ChooseColor.UseVisualStyleBackColor = true;
+			this.ChooseColor.Click += new System.EventHandler(this.ChooseColor_Click);
+			// 
+			// tabPage4
+			// 
+			this.tabPage4.Controls.Add(this.textBox2);
+			this.tabPage4.Controls.Add(this.textBox1);
+			this.tabPage4.Location = new System.Drawing.Point(4, 22);
+			this.tabPage4.Name = "tabPage4";
+			this.tabPage4.Size = new System.Drawing.Size(288, 155);
+			this.tabPage4.TabIndex = 3;
+			this.tabPage4.Text = "Text";
+			this.tabPage4.UseVisualStyleBackColor = true;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(57, 91);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(170, 20);
+			this.textBox2.TabIndex = 1;
+			this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(57, 47);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(170, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(0, 187);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(99, 57);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Apply";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Update);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(105, 187);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(99, 57);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "OK";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Save);
+			// 
+			// button3
+			// 
+			this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.button3.Location = new System.Drawing.Point(210, 187);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(86, 57);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Cancel";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Exit);
+			// 
+			// Form2
+			// 
+			this.AcceptButton = this.button2;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+			this.CancelButton = this.button3;
+			this.ClientSize = new System.Drawing.Size(297, 245);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.tabControl1);
+			this.Name = "Form2";
+			this.Text = "Form2";
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
+			this.tabPage2.ResumeLayout(false);
+			this.tabPage2.PerformLayout();
+			this.tabPage3.ResumeLayout(false);
+			this.tabPage4.ResumeLayout(false);
+			this.tabPage4.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.RadioButton radioButton6;
+		private System.Windows.Forms.RadioButton radioButton5;
+		private System.Windows.Forms.RadioButton radioButton4;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TabPage tabPage3;
+		private System.Windows.Forms.TabPage tabPage4;
+		private System.Windows.Forms.ColorDialog colorDialog1;
+		private System.Windows.Forms.Button ChooseColor;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+	}
+}
